135ee71847b751d6cf9e1978270e90c2e87aaeff
umeneses <umeneses@student.42sp.org.br>
2024-01-03 15:58:39 -0300

    adding more defines

--- headers/so_long.h
+++ headers/so_long.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 15:46:04 by umeneses          #+#    #+#             */
-/*   Updated: 2023/12/20 18:40:57 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/01/03 15:47:17 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 
 # define WIDTH 512
 # define HEIGHT 512
+# define BPP sizeof(int32_t)
 
 typedef struct s_player
 {
