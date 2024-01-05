0061d86741b071882edfe331b0d4444ca2b2b70a
umeneses <umeneses@student.42sp.org.br>
2024-01-05 18:20:29 -0300

    adding new ui canvas

--- headers/so_long.h
+++ headers/so_long.h
@@ -6,13 +6,15 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 15:46:04 by umeneses          #+#    #+#             */
-/*   Updated: 2024/01/03 15:47:17 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/01/05 18:19:01 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	SO_LONG_H
 # define SO_LONG_H
 
+# include "../libs/codam/include/MLX42/MLX42.h"
+# include "../libs/libft/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
@@ -20,6 +22,8 @@
 # define WIDTH 512
 # define HEIGHT 512
 # define BPP sizeof(int32_t)
+# define GRAY_COLOR printf("\033[0;36m")
+# define RESET_COLOR printf("\033[0m")
 
 typedef struct s_player
 {
@@ -27,5 +31,10 @@ typedef struct s_player
 }		t_player;
 
 static mlx_image_t* image;
+mlx_image_t*		cvs_img;
+mlx_image_t*		ship_img;
+mlx_texture_t*		ship_texture;
+
+void	*ft_memset(void *str, int c, size_t size);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,11 +6,10 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 14:47:07 by umeneses          #+#    #+#             */
-/*   Updated: 2024/01/03 15:51:49 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/01/05 18:17:36 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libs/codam/include/MLX42/MLX42.h"
 #include "../headers/so_long.h"
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
@@ -18,7 +17,7 @@ int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
     return (r << 24 | g << 24 | b << 16 | a);
 }
 
-void ft_randomize(void* param)
+void play_random_color(void* param)
 {
 	for (int32_t i = 0; i < image->width; ++i)
 	{
@@ -35,7 +34,7 @@ void ft_randomize(void* param)
 	}
 }
 
-void ft_hook(void* param)
+void keyb_wasd_arrow(void* param)
 {
 	mlx_t*		mlx = param;
 
@@ -59,7 +58,7 @@ void ft_hook(void* param)
 		image->instances[0].x += 5;
 }
 
-void my_keyhook(mlx_key_data_t keydata, void* param)
+void movecounter(mlx_key_data_t keydata, void* param)
 {
 	static t_player	moves;
 
@@ -81,20 +80,26 @@ void my_keyhook(mlx_key_data_t keydata, void* param)
 			\nTotal Moves = %i", ++moves.totalmoves);
 }
 
-void my_scrollhook(double xdelta, double ydelta, void* param)
+void ui_stats_canvas(mlx_t* mlx, mlx_image_t* cvs_img)
 {
-	mlx_key_data_t keydata;
+	// set new image	
+	cvs_img = mlx_new_image(mlx, (WIDTH % 10 * HEIGHT), (HEIGHT % 10) * 10);
+	// set color foreground image
+	ft_memset(cvs_img->pixels, 150, cvs_img->width * cvs_img->height * BPP);
+	mlx_image_to_window(mlx, cvs_img, 0, 0);
+}
 
-	// Simple up or down detection.
-	if (ydelta > 0)
-		puts("Up!");
-	else if (ydelta < 0)
-		puts("Down!");
-	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
-	if (keydata.key == MLX_KEY_LEFT_SHIFT && ydelta > 0)
-		puts("Sliiiide to the left!");
-	else if (keydata.key == MLX_KEY_LEFT_SHIFT && ydelta < 0)
-		puts("Sliiiide to the right!");
+void load_texture(mlx_t* mlx, mlx_texture_t* ship_texture, mlx_image_t* ship_img)
+{
+	// Try to load the file
+	ship_texture = mlx_load_png("./textures/ref-space-shuttle-redux.png");
+	if (!ship_texture)
+		puts(mlx_strerror(mlx_errno));
+
+	// Convert texture to a displayable image
+	ship_img = mlx_texture_to_image(mlx, ship_texture);
+	if (!ship_img || (mlx_image_to_window(mlx, ship_img, 0, 0) < 0))
+		puts(mlx_strerror(mlx_errno));
 }
 
 int32_t main(int32_t argc, const char* argv[])
@@ -102,7 +107,7 @@ int32_t main(int32_t argc, const char* argv[])
 	mlx_t*	mlx;
 
 	// Gotta error check this stuff
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "42_Astronauts_Sooo_Long_Game", true)))
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "42_Astronauts_So_Long_Game", true)))
 	{
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
@@ -119,14 +124,36 @@ int32_t main(int32_t argc, const char* argv[])
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-	
+	ui_stats_canvas(mlx, cvs_img);
+	// load_texture(mlx, ship_texture, ship_img);
 	mlx_image_to_window(mlx, image, 0, 0);
-	mlx_put_pixel(image, 10, 10, 0xFF0000FF);
-	mlx_scroll_hook(mlx, &my_scrollhook, NULL);
-	mlx_loop_hook(mlx, ft_randomize, mlx);
-	mlx_loop_hook(mlx, ft_hook, mlx);
-	mlx_key_hook(mlx, &my_keyhook, NULL);
+	mlx_loop_hook(mlx, play_random_color, mlx);
+	mlx_loop_hook(mlx, keyb_wasd_arrow, mlx);
+	mlx_key_hook(mlx, &movecounter, NULL);
 	mlx_loop(mlx);
+	// mlx_delete_image(mlx, ship_img);
+	// mlx_delete_image(mlx, cvs_img);
+	// mlx_delete_image(mlx, play_img);
+	// mlx_delete_texture(ship_texture);
+	mlx_close_window(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
+
+// mlx_scroll_hook(mlx, &my_scrollhook, NULL);
+
+// void my_scrollhook(double xdelta, double ydelta, void* param)
+// {
+// 	mlx_key_data_t keydata;
+
+// 	// Simple up or down detection.
+// 	if (ydelta > 0)
+// 		puts("Up!");
+// 	else if (ydelta < 0)
+// 		puts("Down!");
+// 	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
+// 	if (keydata.key == MLX_KEY_LEFT_SHIFT && ydelta > 0)
+// 		puts("Sliiiide to the left!");
+// 	else if (keydata.key == MLX_KEY_LEFT_SHIFT && ydelta < 0)
+// 		puts("Sliiiide to the right!");
+// }
