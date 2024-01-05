eaecdbfbf75bf2b83784a1d6ef8416afe378a261
umeneses <umeneses@student.42sp.org.br>
2024-01-05 16:31:48 -0300

    trying to apply texture on player

--- src/main.c
+++ src/main.c
@@ -6,11 +6,10 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 14:47:07 by umeneses          #+#    #+#             */
-/*   Updated: 2024/01/03 15:51:49 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/01/05 16:06:21 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libs/codam/include/MLX42/MLX42.h"
 #include "../headers/so_long.h"
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
@@ -18,11 +17,11 @@ int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
     return (r << 24 | g << 24 | b << 16 | a);
 }
 
-void ft_randomize(void* param)
+void play_random_color(void* param)
 {
-	for (int32_t i = 0; i < image->width; ++i)
+	for (int32_t i = 0; i < play_img->width; ++i)
 	{
-		for (int32_t y = 0; y < image->height; ++y)
+		for (int32_t y = 0; y < play_img->height; ++y)
 		{
 			uint32_t color = ft_pixel(
 				rand() % 0xFF, // R
@@ -30,33 +29,34 @@ void ft_randomize(void* param)
 				rand() % 0xFF, // B
 				rand() % 0xFF  // A
 			);
-			mlx_put_pixel(image, i, y, color);
+			mlx_put_pixel(play_img, i, y, color);
 		}
 	}
 }
 
-void ft_hook(void* param)
+void my_arrowhook(void* param)
 {
-	mlx_t*		mlx = param;
+	mlx_t*	mlx;
 
+	mlx = param;
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-			image->instances[0].y -= 5;
+			play_img->instances[0].y -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-			image->instances[0].y += 5;
+			play_img->instances[0].y += 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
+		play_img->instances[0].x -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
+		play_img->instances[0].x += 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_W))
-			image->instances[0].y -= 5;
+			play_img->instances[0].y -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_S))
-			image->instances[0].y += 5;
+			play_img->instances[0].y += 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_A))
-		image->instances[0].x -= 5;
+		play_img->instances[0].x -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_D))
-		image->instances[0].x += 5;
+		play_img->instances[0].x += 5;
 }
 
 void my_keyhook(mlx_key_data_t keydata, void* param)
@@ -81,52 +81,79 @@ void my_keyhook(mlx_key_data_t keydata, void* param)
 			\nTotal Moves = %i", ++moves.totalmoves);
 }
 
-void my_scrollhook(double xdelta, double ydelta, void* param)
-{
-	mlx_key_data_t keydata;
+// void ui_stats_canvas(mlx_t* mlx, mlx_image_t* cvs_img)
+// {
+// 	// set new image	
+// 	cvs_img = mlx_new_image(mlx, (WIDTH % 10 * HEIGHT), (HEIGHT % 10) * 10);
+// 	// set color foreground image
+// 	ft_memset(cvs_img->pixels, 150, cvs_img->width * cvs_img->height * BPP);
+// 	mlx_image_to_window(mlx, cvs_img, 0, 0);
+// }
 
-	// Simple up or down detection.
-	if (ydelta > 0)
-		puts("Up!");
-	else if (ydelta < 0)
-		puts("Down!");
-	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
-	if (keydata.key == MLX_KEY_LEFT_SHIFT && ydelta > 0)
-		puts("Sliiiide to the left!");
-	else if (keydata.key == MLX_KEY_LEFT_SHIFT && ydelta < 0)
-		puts("Sliiiide to the right!");
-}
+// void load_texture(mlx_t* mlx, mlx_texture_t* ship_texture, mlx_image_t* ship_img)
+// {
+// 	// Try to load the file
+// 	ship_texture = mlx_load_png("./textures/ref-space-shuttle-redux.png");
+// 	if (!ship_texture)
+// 		puts(mlx_strerror(mlx_errno));
+
+// 	// Convert texture to a displayable image
+// 	ship_img = mlx_texture_to_image(mlx, ship_texture);
+// 	if (!ship_img || (mlx_image_to_window(mlx, ship_img, 0, 0) < 0))
+// 		puts(mlx_strerror(mlx_errno));
+// }
 
 int32_t main(int32_t argc, const char* argv[])
 {
 	mlx_t*	mlx;
 
 	// Gotta error check this stuff
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "42_Astronauts_Sooo_Long_Game", true)))
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "42_Astronauts_So_Long_Game", true)))
 	{
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-	if (!(image = mlx_new_image(mlx, 4, 4)))
+	if (!(play_img = mlx_new_image(mlx, 4, 4)))
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	if (mlx_image_to_window(mlx, play_img, 0, 0) == -1)
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
 	
-	mlx_image_to_window(mlx, image, 0, 0);
-	mlx_put_pixel(image, 10, 10, 0xFF0000FF);
-	mlx_scroll_hook(mlx, &my_scrollhook, NULL);
-	mlx_loop_hook(mlx, ft_randomize, mlx);
-	mlx_loop_hook(mlx, ft_hook, mlx);
+	// ui_stats_canvas(mlx, cvs_img);
+	// load_texture(mlx, ship_texture, ship_img);
+	// mlx_scroll_hook(mlx, &my_scrollhook, NULL);
+	mlx_loop_hook(mlx, play_random_color, mlx);
+	mlx_loop_hook(mlx, my_arrowhook, NULL);
 	mlx_key_hook(mlx, &my_keyhook, NULL);
 	mlx_loop(mlx);
+	// mlx_delete_image(mlx, ship_img);
+	// mlx_delete_image(mlx, cvs_img);
+	// mlx_delete_image(mlx, play_img);
+	// mlx_delete_texture(ship_texture);
+	mlx_close_window(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
+
+// void my_scrollhook(double xdelta, double ydelta, void* param)
+// {
+// 	mlx_key_data_t	keydata;
+
+// 	// Simple up or down detection.
+// 	if (ydelta > 0)
+// 		puts("Up!");
+// 	else if (ydelta < 0)
+// 		puts("Down!");
+// 	// Can also detect a mousewheel that go along the X (e.g: MX Master 3)
+// 	if (keydata.key == MLX_KEY_LEFT_SHIFT && ydelta > 0)
+// 		puts("Sliiiide to the left!");
+// 	else if (keydata.key == MLX_KEY_LEFT_SHIFT && ydelta < 0)
+// 		puts("Sliiiide to the right!");
+// }
