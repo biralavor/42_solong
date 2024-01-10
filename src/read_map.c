39649095787007d6d04243b2ddbfb3208b0607a0
umeneses <umeneses@student.42sp.org.br>
2024-01-10 16:52:56 -0300

    adding reading method

---
+++ src/read_map.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 14:29:17 by umeneses          #+#    #+#             */
+/*   Updated: 2024/01/10 14:50:07 by umeneses         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/so_long.h"
+
+char	*read_file(int32_t fd)
+{
+	int32_t	buffer_len;
+	char	*row;
+	char	*buffer;
+
+	buffer_len = 1;
+	row = ft_calloc(1,sizeof(char));
+	buffer = ft_calloc(BUFFERSIZE + 1, sizeof(char));
+	while ((buffer_len > 0))
+	{
+		buffer = read(fd, buffer, BUFFERSIZE);
+		if (buffer < 0)
+			free (buffer);
+		if (buffer == 0)
+			return (row);
+		buffer[buffer_len] = '\0';
+		row = ft_strjoin(row, buffer);
+	}
+	close(fd);
+	return (row);
+}
