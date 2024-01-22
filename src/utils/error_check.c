e53c1be7305016eeb92a9c4ea80dbc15639e53c4
umeneses <umeneses@student.42sp.org.br>
2024-01-22 19:04:50 -0300

    WIP map error check

--- src/utils/error_check.c
+++ src/utils/error_check.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:22:01 by umeneses          #+#    #+#             */
-/*   Updated: 2024/01/22 18:25:49 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/01/22 19:03:58 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,34 +27,22 @@ void	error_check(char **argv)
 	ft_printf("inside function filename_len = %d\n", filename_len);
 	ft_printf("inside function filename = %s\n", argv[1]);
 	extension_name = ft_strnstr(argv[1], ".", filename_len);
-	ft_printf("extension_name pos%d ANTES WHILE = %c\n", index, extension_name[index]);
 	while (extension_name != '\0')
 	{
 		if (extension_name[index] == '.')
 		{
-			ft_printf("extension_name pos%d = %c\n", index, extension_name[index]);
-			index++;
-			if (extension_name[index] == 'b')
+			if (extension_name[++index] == 'b')
 			{
-				ft_printf("extension_name pos%d = %c\n", index, extension_name[index]);
-				index++;
-				if (extension_name[index] == 'e')
+				if (extension_name[++index] == 'e')
 				{
-					ft_printf("extension_name pos%d = %c\n", index, extension_name[index]);
-					index++;
-					if (extension_name[index] == 'r')
-					{
-						ft_printf("extension_name pos%d = %c\n", index, extension_name[index]);
+					if (extension_name[++index] == 'r')
 						break;
-					}
 				}
 			}
 		}
 		else
 			ft_printf("\nstrRchar error\n");
 	}
-	
-	
 }
 
-// char	*ft_strnstr(const char *big, const char *tofind, size_t len)
+// change to boll
