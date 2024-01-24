f85d2c2b13e866d912c5718b241e4575525ef96f
umeneses <umeneses@student.42sp.org.br>
2024-01-24 14:16:22 -0300

    map extension check function OK

--- src/utils/errors_check.c
+++ src/utils/errors_check.c
@@ -1,48 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_check.c                                      :+:      :+:    :+:   */
+/*   errors_check.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:22:01 by umeneses          #+#    #+#             */
-/*   Updated: 2024/01/22 19:03:58 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/01/23 10:53:15 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	error_check(char **argv)
+bool	map_extension_check(char **argv)
 {
 	size_t	extension_len;
 	size_t	filename_len;
-	char	extension_char;
 	char	*extension_name;
-	char	*filename;
 	size_t	index;
 
 	index = 0;
 	filename_len = ft_strlen(argv[1]);
-	filename = argv[1];
-	ft_printf("inside function filename_len = %d\n", filename_len);
-	ft_printf("inside function filename = %s\n", argv[1]);
 	extension_name = ft_strnstr(argv[1], ".", filename_len);
-	while (extension_name != '\0')
+	extension_len = ft_strlen(extension_name);
+	while (extension_name != (void *)0)
 	{
-		if (extension_name[index] == '.')
-		{
-			if (extension_name[++index] == 'b')
-			{
-				if (extension_name[++index] == 'e')
-				{
-					if (extension_name[++index] == 'r')
-						break;
-				}
-			}
-		}
-		else
-			ft_printf("\nstrRchar error\n");
+		if (extension_len == 4)
+			if (extension_name[index] == '.')
+				if (extension_name[++index] == 'b')
+					if (extension_name[++index] == 'e')
+						if (extension_name[++index] == 'r')
+							return (true);
+		return (ft_putendl_fd("\nGame Map file extension too long.\n \
+							It needs to be '.ber'\n", STDOUT_FILENO), false);
+		break ;
 	}
 }
-
-// change to boll
