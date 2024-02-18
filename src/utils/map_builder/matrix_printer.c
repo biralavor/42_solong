e57f4042d406bcf2258e84093d0af42d605cb946
umeneses <umeneses@student.42.fr>
2024-02-18 16:08:03 -0300

    simplifying map matrix printing on terminal to fast tests

--- src/utils/map_builder/matrix_printer.c
+++ src/utils/map_builder/matrix_printer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   matrix_printer.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bira <bira@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 16:56:35 by umeneses          #+#    #+#             */
-/*   Updated: 2024/01/30 17:02:02 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/02/16 21:43:14 by bira             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 void	matrix_printer(t_map *map)
 {
-	int32_t x = 0;
-	while (x <= map->height - 1)
-	{
-		int32_t	y = 0;
-		while (++y <= map->width)
-			ft_printf("%c", map->matrix[x][y]);
-		x++;
-	}
+	int32_t	y;
+
+	y = -1;
+	while (++y <= map->height - 1)
+		ft_printf("%s\n", map->matrix[y]);
 }
