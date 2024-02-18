d3027699c7865eecaf9f0f1bb1d7d8fc20f98747
umeneses <umeneses@student.42.fr>
2024-02-18 16:12:53 -0300

    reducing main function size

--- src/main.c
+++ src/main.c
@@ -6,13 +6,25 @@
 /*   By: bira <bira@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 14:47:07 by umeneses          #+#    #+#             */
-/*   Updated: 2024/02/16 14:21:39 by bira             ###   ########.fr       */
+/*   Updated: 2024/02/16 22:18:39 by bira             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int32_t main(int32_t argc, char **argv)
+bool	map_init(char **argv, t_game *game)
+{
+	if (map_extension_approved(argv) == true &&		\
+			map_opener(argv, game->map) == true &&		\
+			map_size_approved(game->map) == true &&		\
+			map_contens_checker(game->map) == true)
+			return (true);
+	free_map(game->map);
+	free (game);
+	return (false);
+}
+
+int32_t	main(int32_t argc, char **argv)
 {
 	t_game	*game;
 
@@ -20,15 +32,12 @@ int32_t main(int32_t argc, char **argv)
 	game->map = (t_map *)ft_calloc(1, sizeof(t_map));
 	if (argc == 2)
 	{
-		if (map_extension_approved(argv) == true &&		\
-			map_opener(argv, game->map) == true &&		\
-			map_size_approved(game->map) == true &&		\
-			map_contens_checker(game->map) == true)
-		{
+		if (map_init(argv, game) == true)
 			ft_printf("\nmap approved!\n");
-			// free (&game.map->line);
-			// close (&game.map->fd);
-		}
+		matrix_printer(game->map);
+		// game = structures_init();
+		game->mlx = mlx_init(WIDTH, HEIGHT, "42_Astronauts_So_Long_Game", true);
+		displaying_sprites(game);
 	}
 	else if (argc > 2)
 		return (ft_putendl_fd("Error\nToo many arguments, buddy.", \
@@ -36,11 +45,12 @@ int32_t main(int32_t argc, char **argv)
 	else
 		return (ft_putendl_fd("Ops!\nWhere is your map?", \
 				STDOUT_FILENO), EXIT_FAILURE);
+	free_map(game->map);
+	free (game);
 	return (0);
 }
 
-// game = structures_init();
-// game->mlx = mlx_init(WIDTH, HEIGHT, "42_Astronauts_So_Long_Game", true);
+
 // load_texture(game);
 // ui_stats_canvas(game);
 // game->ship_img = mlx_new_image(game->mlx, 48, 39);
