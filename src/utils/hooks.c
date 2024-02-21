42449368d812fe8da60389e3e009161fa403f50e
umeneses <umeneses@student.42sp.org.br>
2024-02-21 16:55:52 -0300

    cleaning old code

--- src/utils/hooks.c
+++ src/utils/hooks.c
@@ -6,59 +6,24 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 16:52:24 by umeneses          #+#    #+#             */
-/*   Updated: 2024/01/25 16:52:25 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/02/21 16:51:38 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
-// static void error(void)
-// {
-// 	puts(mlx_strerror(mlx_errno));
-// 	exit(EXIT_FAILURE);
-// }
-
-// int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
-// {
-//     return (r << 24 | g << 24 | b << 16 | a);
-// }
-
-// void play_random_color(void *param)
-// {
-// 	t_game	*game;
-
-// 	game = param;
-// 	for (int32_t i = 0; i < game->play_img->width; ++i)
-// 	{
-// 		for (int32_t y = 0; y < game->play_img->height; ++y)
-// 		{
-// 			uint32_t color = ft_pixel(
-// 				rand() % 0xFF, // R
-// 				rand() % 0xFF, // G
-// 				rand() % 0xFF, // B
-// 				rand() % 0xFF  // A
-// 			);
-// 			mlx_put_pixel(game->pl#include "so_long.h"ay_img, i, y, color);
-// 		}
-// 	}
-// }
-
-// void keyb_wasd_arrow(void* param)
-// {
-// 	t_game	*game;
-	
-// 	game = param;
-// 	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
-// 		mlx_close_window(game->mlx);
-// 	if (mlx_is_key_down(game->mlx, (MLX_KEY_UP || MLX_KEY_W)))
-// 			game->play_img->instances[0].y -= 5;
-// 	if (mlx_is_key_down(game->mlx, (MLX_KEY_DOWN || MLX_KEY_S)))
-// 			game->play_img->instances[0].y += 5;
-// 	if (mlx_is_key_down(game->mlx, (MLX_KEY_LEFT || MLX_KEY_A)))
-// 		game->play_img->instances[0].x -= 5;
-// 	if (mlx_is_key_down(game->mlx, (MLX_KEY_RIGHT || MLX_KEY_D)))
-// 		game->play_img->instances[0].x += 5;
-// }
+void keyb_wasd_arrow(t_game *game, void* param)
+{
+	game = param;
+	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(game->mlx);
+	if (mlx_is_key_down(game->mlx, (MLX_KEY_UP || MLX_KEY_W)))
+			game->play_img->instances[0].y -= 5;
+	if (mlx_is_key_down(game->mlx, (MLX_KEY_DOWN || MLX_KEY_S)))
+			game->play_img->instances[0].y += 5;
+	if (mlx_is_key_down(game->mlx, (MLX_KEY_LEFT || MLX_KEY_A)))
+		game->play_img->instances[0].x -= 5;
+	if (mlx_is_key_down(game->mlx, (MLX_KEY_RIGHT || MLX_KEY_D)))
+		game->play_img->instances[0].x += 5;
+}
 
 // void movecounter(mlx_key_data_t keydata, void* param)
 // {
@@ -95,4 +60,31 @@
 // {
 // 	static t_game game;
 // 	return (&game);
+// }
+
+
+/* PUT PIXEL RANDOM COLOR */
+// int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+// {
+//     return (r << 24 | g << 24 | b << 16 | a);
+// }
+
+// void play_random_color(void *param)
+// {
+// 	t_game	*game;
+
+// 	game = param;
+// 	for (int32_t i = 0; i < game->play_img->width; ++i)
+// 	{
+// 		for (int32_t y = 0; y < game->play_img->height; ++y)
+// 		{
+// 			uint32_t color = ft_pixel(
+// 				rand() % 0xFF, // R
+// 				rand() % 0xFF, // G
+// 				rand() % 0xFF, // B
+// 				rand() % 0xFF  // A
+// 			);
+// 			mlx_put_pixel(game->pl#include "so_long.h"ay_img, i, y, color);
+// 		}
+// 	}
 // }
