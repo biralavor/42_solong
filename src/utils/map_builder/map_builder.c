0cf15980877dac4f00eff10800dbbd1cd99d9cb3
umeneses <umeneses@student.42sp.org.br>
2024-02-23 18:13:33 -0300

    cleaning code

--- src/utils/map_builder/map_builder.c
+++ src/utils/map_builder/map_builder.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:50:18 by umeneses          #+#    #+#             */
-/*   Updated: 2024/02/23 16:14:16 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/02/23 16:22:50 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,8 @@ void	parse_imgtomap(t_map *map, char toparse, mlx_t *mlx, mlx_image_t *img)
 		while (++x <= map->width)
 		{
 			if (toparse == map->matrix[y][x])
-			{
-				ft_printf("matrix[%d][%d] = %c\n", y, x, map->matrix[y][x]);
 				if (mlx_resize_image(img, PIXEL_SIZE, PIXEL_SIZE) == true)
 					mlx_image_to_window(mlx, img, y * PIXEL_SIZE, x * PIXEL_SIZE);
-			}
 		}
 	}
 }
