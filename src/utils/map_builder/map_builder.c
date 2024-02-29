ca435ce6d4891c96513c5e8e1a109f018a407cbb
umeneses <umeneses@student.42sp.org.br>
2024-02-29 16:52:08 -0300

    first lines for background only, others for map itens

--- src/utils/map_builder/map_builder.c
+++ src/utils/map_builder/map_builder.c
@@ -6,26 +6,40 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:50:18 by umeneses          #+#    #+#             */
-/*   Updated: 2024/02/23 16:22:50 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/02/29 16:49:52 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	parse_imgtomap(t_map *map, char toparse, mlx_t *mlx, mlx_image_t *img)
+void	parse_imgtomap(t_map *map, char toparse, mlx_t *mlx, mlx_image_t **img)
 {
 	int32_t	x;
 	int32_t	y;
 
 	y = -1;
-	while ((++y <= map->height - 1) && map->matrix)
-	{
-		x = -1;
-		while (++x <= map->width)
+	if (toparse == 'b')
+		while (++y < HEIGHT)
 		{
-			if (toparse == map->matrix[y][x])
-				if (mlx_resize_image(img, PIXEL_SIZE, PIXEL_SIZE) == true)
-					mlx_image_to_window(mlx, img, y * PIXEL_SIZE, x * PIXEL_SIZE);
+			x = -1;
+			while (++x < WIDTH)
+			{
+				if (mlx_resize_image(*img, PIXEL_SIZE, PIXEL_SIZE) == true)
+					mlx_image_to_window(mlx, *img, y * PIXEL_SIZE, x * PIXEL_SIZE);
+			}
+		}
+	else
+	{
+		while ((++y <= map->height - 1) && map->matrix)
+		{
+			x = -1;
+			while (++x <= map->width)
+			{
+				if (toparse == map->matrix[y][x])
+					if (mlx_resize_image(*img, PIXEL_SIZE, PIXEL_SIZE) == true)
+						mlx_image_to_window(mlx, *img, y * PIXEL_SIZE, x * PIXEL_SIZE);
+			}
 		}
 	}
+
 }
