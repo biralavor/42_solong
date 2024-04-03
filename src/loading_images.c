c164d7bcb0f1ac09fe55b9272e515b58df2a66df
umeneses <umeneses@student.42sp.org.br>
2024-04-03 19:19:22 -0300

    spliting functions for animation feature

--- src/loading_images.c
+++ src/loading_images.c
@@ -6,11 +6,10 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:35:34 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/03 12:40:50 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:38:11 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "MLX42.h"
 #include "so_long.h"
 
 bool	loading_png(char *imagepath, mlx_texture_t **texture)
@@ -29,33 +28,39 @@ bool	preparing_image(mlx_t *mlx, mlx_texture_t *texture, mlx_image_t **img)
 	return (true);
 }
 
-void	displaying_sprites(mlx_t *mlx, t_sprite *sprites, t_map *map)
+void	render_fixedsprites(mlx_t *mlx, t_sprite *sprites, t_map *map)
 {
-	// t_sprite *sprites_temp;
-
-	// sprites_temp = (t_sprite *)ft_calloc(1, sizeof(t_sprite));
-	// if (!sprites_temp)
-	// 	free (sprites_temp);
-	// sprites = sprites_temp;
 	if (loading_png(SPACEFIELD, &sprites->back_tt)
 		&& preparing_image(mlx, sprites->back_tt, &sprites->back)
 		&& loading_png(BOUNDARY, &sprites->border_tt)
-		&& preparing_image(mlx, sprites->border_tt, &sprites->border)
-		&& loading_png(SPACESHIP, &sprites->player_tt)
+		&& preparing_image(mlx, sprites->border_tt, &sprites->border))
+		{
+			parse_imgtomap(map, 'b', mlx, sprites->back);
+			parse_imgtomap(map, '1', mlx, sprites->border);
+		}
+	else
+		{
+			ft_putendl_fd("Fixed Sprites didn't load correctly.\n", STDOUT_FILENO);
+			return;
+		}
+}
+
+void	render_movingsprites(mlx_t *mlx, t_sprite *sprites, t_map *map)
+{
+	if (loading_png(SPACESHIP, &sprites->player_tt)
 		&& preparing_image(mlx, sprites->player_tt, &sprites->player)
 		&& loading_png(ASTRONAUT, &sprites->coin_tt)
 		&& preparing_image(mlx, sprites->coin_tt, &sprites->coin)
 		&& loading_png(EARTH, &sprites->exit_tt)
 		&& preparing_image(mlx, sprites->exit_tt, &sprites->exit))
-		parse_imgtomap(map, 'b', mlx, sprites->back);
-		parse_imgtomap(map, '1', mlx, sprites->border);
-		parse_imgtomap(map, 'P', mlx, sprites->player);
-		parse_imgtomap(map, 'C', mlx, sprites->coin);
-		parse_imgtomap(map, 'E', mlx, sprites->exit);
-	// free(sprites_temp);
-// 	else
-// 		{
-// 			ft_putendl_fd("Displaying Sprites didn't load correctly.\n", STDOUT_FILENO);
-// 			return;
-// 		}
+		{
+			parse_imgtomap(map, 'P', mlx, sprites->player);
+			parse_imgtomap(map, 'C', mlx, sprites->coin);
+			parse_imgtomap(map, 'E', mlx, sprites->exit);
+		}
+	else
+		{
+			ft_putendl_fd("Moving Sprites didn't load correctly.\n", STDOUT_FILENO);
+			return;
+		}
 }
