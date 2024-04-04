da473b20d561454f8f2760664eefb724538e6e3a
umeneses <umeneses@student.42sp.org.br>
2024-04-04 17:01:03 -0300

    now displaying on terminal the acual positions of player and coins

--- src/utils/gameplay.c
+++ src/utils/gameplay.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 10:08:04 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/04 15:31:38 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/04 16:59:52 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,17 @@ void	ft_collectable(t_game *game)
 {
 	int32_t	index;
 
-	ft_printf("\nPlayer_pos[%i][%i]", game->map->matrix
-	[game->sprites->player->instances->x / PIXEL_SIZE]
-	[game->sprites->player->instances->y / PIXEL_SIZE]);
 	index = 0;
-	while (index <= game->map->coin_index)
+	ft_printf("\nPlayer_y[%i]x?[%i]", 
+	(game->sprites->player->instances->y / PIXEL_SIZE),
+	(game->sprites->player->instances->x / PIXEL_SIZE));
+	while (index < game->map->coin_index)
 	{
-		// ft_printf("\nCoin[%i]_pos[%i][%i]", index,
-		// game->map->matrix[game->sprites->coin->instances[index].x / PIXEL_SIZE]
-		// [game->sprites->coin->instances[index].y / PIXEL_SIZE]);
+		ft_printf("\nCoin[%i]_pos_y[%i]x[%i]", index, 
+		(game->sprites->coin->instances[index].y / PIXEL_SIZE), 
+		(game->sprites->coin->instances[index].x / PIXEL_SIZE));
+		// ft_printf("\nCoin[%i]_count[%i]", index,
+		// game->map->matrix[game->sprites->coin->count]);
 		index++;
 	}
 	
