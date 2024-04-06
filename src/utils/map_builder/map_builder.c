cdf9cfa746ffa97adcc3d377fdd3ed375f46f6dc
umeneses <umeneses@student.42sp.org.br>
2024-04-06 16:06:33 -0300

    fixing placing items on the map. X and Y was inverted in mlx

--- src/utils/map_builder/map_builder.c
+++ src/utils/map_builder/map_builder.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 16:50:18 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/04 11:25:40 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/06 16:05:01 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,24 @@ void	parse_imgtomap(t_map *map, char toparse, mlx_t *mlx, mlx_image_t *img)
 			while (++x < WIDTH)
 			{
 				if (mlx_resize_image(img, PIXEL_SIZE, PIXEL_SIZE))
-					mlx_image_to_window(mlx, img, (y * PIXEL_SIZE), \
-										(x * PIXEL_SIZE));
+					mlx_image_to_window(mlx, img, (x * PIXEL_SIZE), \
+										(y * PIXEL_SIZE));
 			}
 		}
 	else
 	{
+		y = -1;
 		while ((++y <= map->height - 1) && map->matrix)
 		{
 			x = -1;
 			while (++x <= map->width)
 			{
 				if (toparse == map->matrix[y][x])
+				{
 					if (mlx_resize_image(img, PIXEL_SIZE, PIXEL_SIZE))
-						mlx_image_to_window(mlx, img, (y * PIXEL_SIZE), \
-										(x * PIXEL_SIZE));
+						mlx_image_to_window(mlx, img, (x * PIXEL_SIZE), \
+										(y * PIXEL_SIZE));
+				}
 			}
 		}
 	}
