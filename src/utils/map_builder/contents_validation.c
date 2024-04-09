210ecdf4b2953044c94c6d74fb6957f576c973ec
umeneses <umeneses@student.42sp.org.br>
2024-04-09 17:46:46 -0300

    applying norm, fixing if/else conditions

--- src/utils/map_builder/contents_validation.c
+++ src/utils/map_builder/contents_validation.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 13:38:49 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/09 15:31:44 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/09 17:43:31 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,34 @@ bool	map_contents_checker(t_map *map)
 {
 	if (has_walls(map))
 	{
-		if (has_specific_char(map, 'P', 1)
-			&& has_specific_char(map, 'E', 1)
-			&& has_specific_char(map, 'C', 999))
+		if ((has_specific_char(map, 'P', 1))
+			&& (has_specific_char(map, 'E', 1))
+			&& (has_specific_char(map, 'C', 999)))
 		{
 			if (!is_char_locked(map) && !has_illegal_char(map))
 			{
 				how_many_walls(map);
+				ft_printf("\nspecific char APPROVED\n");
 				return (true);
 			}
 		}
 	}
+	ft_printf("\n\nspecific char NOT passed\n");
 	return (false);
 }
 
 bool	has_illegal_char(t_map *map)
 {
 	char	found;
-	int32_t	x;
-	int32_t	y;
 
 	found = '\0';
-	y = -1;
-	while ((++y <= map->height - 1) && map->matrix)
+	map->read_y = -1;
+	while ((++map->read_y <= map->height - 1) && map->matrix)
 	{
-		x = -1;
-		while (++x <= map->width - 1)
+		map->read_x = -1;
+		while (++map->read_x <= map->width - 1)
 		{
-			found = map->matrix[y][x];
+			found = map->matrix[map->read_y][map->read_x];
 			if (!(found == '1' || found == '0'
 					|| found == 'P' || found == 'E' || found == 'C'))
 			{
@@ -62,28 +62,23 @@ bool	has_specific_char(t_map *map, char tofind, int limiter)
 	int32_t	found;
 
 	found = 0;
+	map->read_y = -1;
 	while ((++map->read_y <= map->height - 1) && map->matrix)
 	{
+		map->read_x = -1;
 		while (++map->read_x <= map->width - 1)
 		{
 			if (tofind == map->matrix[map->read_y][map->read_x])
 				found++;
 		}
-		if ((map->read_y == map->height - 1) && (map->read_x == map->width))
+		if ((map->read_y == map->height - 1) && (map->read_x == map->width -1))
 			break ;
 	}
 	map->coin_index = found;
-	if (tofind == 'C' && found == 0)
+	if ((tofind == 'C' && found == 0) || (tofind != 'C' && found != limiter))
 	{
-		ft_putendl_fd("\n1-Error.\nItems on MAP doesn't match requirements: \
-						\nPlayer = 1\nExit = 1\nCollectables = 1 or more...", \
-						STDOUT_FILENO);
-		return (false);
-	}
-	else if (found > limiter)
-	{
-		ft_putendl_fd("\nError.\nItems on MAP doesn't match requirements: \
-			\nPlayer = 1\nExit = 1\nCollectables = 1 or more...", \
+		ft_putendl_fd("\nError.\nItems on MAP doesn't match requirements:"
+			"\nPlayer = 1\nExit = 1\nCollectables = 1 or more...",
 			STDOUT_FILENO);
 		return (false);
 	}
@@ -95,8 +90,10 @@ bool	is_char_locked(t_map *map)
 	char	item;
 
 	item = '\0';
+	map->read_y = -1;
 	while ((++map->read_y <= map->height - 1) && map->matrix)
 	{
+		map->read_x = -1;
 		while (++map->read_x <= map->width)
 		{
 			item = map->matrix[map->read_y][map->read_x];
